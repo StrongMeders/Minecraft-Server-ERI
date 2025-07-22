b965c9a99a91e06166b611b6464ea6d0d141185a
StrongMeders <strongmeders@gmail.com>
2025-07-22 19:46:52 -0300

    re-adicionando funcionamento do NGrok

--- iniciador.py
+++ iniciador.py
@@ -3,25 +3,29 @@ import time
 import os
 from datetime import datetime
 import getpass
-from pyngrok import ngrok
+from pyngrok import ngrok, conf
+from dotenv import load_dotenv
+
 
 # CONFIGURAÇÕES
 SERVER_DIR = os.path.join(os.getcwd(), "server")
 SERVER_JAR = "server.jar"
 MINECRAFT_PORT = 25565
 
-def git_sync():
-    print("[Git] Sincronizando repositório com origin/main...")
-    result_fetch = subprocess.run("git fetch origin", shell=True, cwd=os.getcwd())
-    if result_fetch.returncode != 0:
-        print("[Git] Erro ao fazer fetch!")
-        exit(1)
+load_dotenv()   # Carrega as variáveis do .env
 
-    result_reset = subprocess.run("git reset --hard origin/main", shell=True, cwd=os.getcwd())
-    if result_reset.returncode != 0:
-        print("[Git] Erro ao fazer reset!")
-        exit(1)
-    print("[Git] Repositório sincronizado com sucesso!")
+# def git_sync():
+#     print("[Git] Sincronizando repositório com origin/main...")
+#     result_fetch = subprocess.run("git fetch origin", shell=True, cwd=os.getcwd())
+#     if result_fetch.returncode != 0:
+#         print("[Git] Erro ao fazer fetch!")
+#         exit(1)
+
+#     result_reset = subprocess.run("git reset --hard origin/main", shell=True, cwd=os.getcwd())
+#     if result_reset.returncode != 0:
+#         print("[Git] Erro ao fazer reset!")
+#         exit(1)
+#     print("[Git] Repositório sincronizado com sucesso!")
 
 
 # def iniciar_ngrok():
@@ -29,6 +33,28 @@ def git_sync():
 #     tcp_tunnel = ngrok.connect(addr=MINECRAFT_PORT, proto="tcp")
 #     print(f"[Ngrok] Endereço: {tcp_tunnel.public_url}")
 #     return tcp_tunnel.public_url
+
+
+def iniciar_ngrok():
+    print("[Ngrok] Iniciando túnel TCP via pyngrok...")
+
+    token = os.getenv("NGROK_AUTHTOKEN")
+    if not token:
+        print("[Ngrok] Token não encontrado nas variáveis de ambiente!")
+        exit(1)
+
+    conf.get_default().auth_token = token
+    tcp_tunnel = ngrok.connect(addr=25565, proto="tcp")
+
+    url = tcp_tunnel.public_url  # Exemplo: tcp://0.tcp.ngrok.io:19342
+    host, port = url.replace("tcp://", "").split(":")
+
+    print("\n[Servidor Online]")
+    print(f"ip: {host}:{port}\n")
+
+    return host, port
+
+#Iniciar servidor
 
 def iniciar_server():
     jar_path = os.path.join(SERVER_DIR, SERVER_JAR)
@@ -39,22 +65,22 @@ def iniciar_server():
     processo = subprocess.Popen(f'java -jar "{jar_path}" nogui', cwd=SERVER_DIR, shell=True)
     return processo
 
-def git_push(usuario):
-    print("[Git] Fazendo commit e push do backup...")
-    datahora = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-    msg_commit = f"{datahora}-{usuario}"
-    subprocess.run("git add .", shell=True, cwd=os.getcwd())
-    subprocess.run(f'git commit -m "{msg_commit}"', shell=True, cwd=os.getcwd())
-    subprocess.run("git push", shell=True, cwd=os.getcwd())
-    print("[Git] Backup enviado!")
+# def git_push(usuario):
+#     print("[Git] Fazendo commit e push do backup...")
+#     datahora = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+#     msg_commit = f"{datahora}-{usuario}"
+#     subprocess.run("git add .", shell=True, cwd=os.getcwd())
+#     subprocess.run(f'git commit -m "{msg_commit}"', shell=True, cwd=os.getcwd())
+#     subprocess.run("git push", shell=True, cwd=os.getcwd())
+#     print("[Git] Backup enviado!")
 
 def main():
     usuario = getpass.getuser()
-    git_sync()
-    # iniciar_ngrok()
+    # git_sync()
+    iniciar_ngrok()
     processo = iniciar_server()
     processo.wait()
-    git_push(usuario)
+    # git_push(usuario)
 
 if __name__ == "__main__":
     main()
